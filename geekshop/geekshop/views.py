cc3e244410c85968985537dedb26a17842a5ee71
Pavel <gorobecp@gmail.com>
2021-06-02 21:41:50 +0500

    lesson_03

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from mainapp.models import Product
 
 
-def index(request):
+def index(request, pk=None):
+
     title = 'geekshop'
+    products = Product.objects.all()[:4]
+
     context = {
+        'products': products,
         'title': title,
     }
     return render(request, 'geekshop/index.html', context=context)
