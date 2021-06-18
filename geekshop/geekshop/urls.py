e239f9c561f7342f163f86ca0bf678461e5c3e9a
Pavel <gorobecp@gmail.com>
2021-06-18 21:53:44 +0500

    lesson_07

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -22,9 +22,11 @@ from django.conf.urls.static import static
 from authapp import urls as authapp_urls
 from mainapp import urls as mainapp_urls
 from basketapp import urls as basketapp_urls
+from adminapp import urls as adminapp_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin_staff/', include(adminapp_urls, namespace='admin_staff'), name='admin_staff'),
 
     # path('products/', include(urls, namespace='products'), name='products'),
     path('auth/', include(authapp_urls, namespace='auth'), name='auth'),
